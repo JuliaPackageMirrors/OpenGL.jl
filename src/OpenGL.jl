fac9e2651f578abff2b53fed326944bd50a9bd53
Andrew Ladouceur <andy@drub.ca>
2014-02-28 14:19:16 -0800

    Added comment explaining OpenGLver section.

--- src/OpenGL.jl
+++ src/OpenGL.jl
@@ -1,6 +1,10 @@
 #  Jasper den Ouden 02-08-2012
 # Placed in public domain.
 
+
+# Supports use of defining an 'OpenGLver' string before importing the package,
+# kept in place for backwards compability. New code should use the @version
+# macro method, instead.
 const OPENGL_VERSIONS = "1.0", "2.1", "3.2", "3.3", "4.2", "4.3"
 if isdefined(:OpenGLver) && OpenGLver in OPENGL_VERSIONS
     OpenGLmod = string("gl", replace(OpenGLver, ".", ""))
