154292c5e0361297bd2e2980f0864440a18ee7a0
Andrew Ladouceur <andy@drub.ca>
2014-03-02 10:40:59 -0800

    Fixed scope issue with version macro.
    Due to Julia's importing mechanisms, it's impossible to use a builtin
    like include() or require() to import code (and eval it) within the
    context of a module that isn't Main.
    
    The only path I can find around this problem is to make the same calls
    that the require() function makes, but do it ourselves within the OpenGL
    module context.
    
    This fixes the issue with the `OpenGLStd` module ending up in the Main
    namespace, instead of the OpenGL namespace. However, any attempt to
    export these symbols throws them into the Main namespace. The current
    implementation omits the export call, ensuring that this:
    
    > using OpenGL
    > @OpenGL.version "3.2"
    
    Only ever populates the OpenGL module's namespace. It unfortunately also
    prevents the use of a `using OpenGL` or `importall OpenGL` call to bring
    the symbols into another namespace.
    
    Using a custom macro for the import would resolve this, but it does mean
    our OpenGL module never appears like a full, regular module, as it
    doesn't export any symbols itself.
    
    Alternatively, determining how to alter the OpenGL module so that its
    exports include those of the modules we dynamically add to it would
    allow for more natural behaviour.

--- src/OpenGL.jl
+++ src/OpenGL.jl
@@ -23,9 +23,18 @@ macro version(v)
         local v = replace($v, ".", "")
         local file = "OpenGL/src/gl$(v)/gl$(v)"
 
-        eval(require(file))
-        eval(Expr(:using, :OpenGLStd))
-        eval(Expr(:export, names(eval(:OpenGLStd))...))
+        # These two Base library calls are being used instead of just
+        # require()-ing the file because the require() function itself has a
+        # hardcoded context value of 'Main' when it evals the file contents.
+        #
+        # The only workaround I can think of is to make the same call that
+        # the require function makes, ourselves, in the context of our module.
+        OpenGL.eval(:(Base.include_from_node1(Base.find_in_path($file))))
+
+        # Unlike the require() function, the `using` builtin accepts a module
+        # context to operate within.
+        OpenGL.eval(Expr(:using, :OpenGL, :OpenGLStd))
+
     end
 end
 
